cfdb047328c75e9395d1ef1079f0eee87879b2c1
Stefano <stefano.forti92@gmail.com>
2025-05-20 15:28:27 +0200

    main

---
+++ app.py
@@ -0,0 +1,72 @@
+import streamlit as st
+import pydeck as pdk
+import pandas as pd
+
+# Dati dei visitatori
+visitors = [
+    {'name': 'Prof. Smith', 'lat': 51.5074, 'lon': -0.1278, 'affiliation': 'UCL'},
+    {'name': 'Dr. Rossi', 'lat': 43.7167, 'lon': 10.4, 'affiliation': 'UniPi'},
+]
+
+# Creazione del DataFrame
+df = pd.DataFrame(visitors)
+
+
+# Definizione dell'icona personalizzata
+icon_data = {
+    "url": "https://png.pngtree.com/png-vector/20220630/ourmid/pngtree-location-pin-point-marker-placeholder-png-image_5326979.png",
+    "width":65,
+    "height": 65,
+    "anchorY": 65,
+}
+
+# Aggiunta delle informazioni sull'icona al DataFrame
+df["icon_data"] = None
+for i in df.index:
+    df.at[i, "icon_data"] = icon_data
+
+# Definizione del layer con icone personalizzate
+icon_layer = pdk.Layer(
+    type="IconLayer",
+    data=df,
+    get_icon="icon_data",
+    get_size=2,
+    size_scale=8,
+    get_position="[lon, lat]",
+    pickable=True,
+)
+
+# Definizione della vista iniziale
+view_state = pdk.ViewState(
+    latitude=0,
+    longitude=0,
+    zoom=0.5,    # più basso = vista globale
+    pitch=0,
+)
+
+# Creazione della mappa
+r = pdk.Deck(
+    layers=[icon_layer],
+    initial_view_state=view_state,
+    tooltip={"text": "{name} ({affiliation})"},
+    map_style='https://basemaps.cartocdn.com/gl/positron-gl-style/style.json'  # <- stile chiaro!
+)
+
+
+cherubino_url = "https://www.unipi.it/wp-content/uploads/Raggruppa-3020.svg"
+
+st.markdown(
+    f"""
+    <div style="display: flex; align-items: center; gap: 1rem; margin-bottom: 1rem;">
+        <img src="{cherubino_url}" alt="Cherubino UniPi" width="200">
+        Dipartimento di Informatica<br>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+
+#st.title("Mappa dei Visitatori")
+st.write("Questa è una mappa dei visitatori del Dipartimento di Informatica dell'Università di Pisa.")
+
+# Visualizzazione della mappa in Streamlit
+st.pydeck_chart(r)
